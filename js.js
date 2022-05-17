2ed3e967d2ea17ea3c4607d5866366adc07f547f
Rade473 <r.shekularats@gmail.com>
2022-05-17 16:53:26 +0200

    Fixed grid and added color change

--- js.js
+++ js.js
@@ -1,10 +1,59 @@
-const canvas = document.getElementById('drawing')
+document.addEventListener('DOMContentLoaded', function(){
+  slidernumber = parseInt(slider.value)
+  createCanvas(slidernumber*slidernumber)
+  draw()
 
-function createCanvas(n){
-    for (i=0; i<n;i++){
-       const div = document.createElement('div');
+}
+)
+
+const canvas = document.getElementById('drawing')
+const slider = document.getElementById('myRange');
+const sizevalue = document.getElementById('sizevalue')
+const clearbtn = document.getElementById('clear')
+const color = document.getElementById('colorpicker')
+const activedrawing = document.getElementsByClassName('activedrawing')
+const drawblock = document.getElementsByClassName('drawingelement')
+
+
+
+sizevalue.innerHTML = `${slider.value}x${slider.value}`;
+slider.oninput = function changeSlider() {
+    sizevalue.innerHTML = `${this.value}x${this.value}`;
+    slidernumber = parseInt(slider.value)
+  
+  }
+slider.onmouseup = function changeCanvas(){
+  size=slidernumber*slidernumber
+    clearCanvas()
+    createCanvas(size);
+}
+
+clearbtn.addEventListener('click',clearCanvas)
+function clearCanvas(){
+   canvas.innerHTML = '';
+   
+}
+
+function createCanvas(size){
+    for (i=0; i<size;i++){
+        const div = document.createElement('div');
         canvas.appendChild(div)
-        div.classList.add('activedrawing')
+        div.classList.add('drawingelement')
+        document.documentElement.style.setProperty('--size', `${slidernumber}`)
     }
 }
+
+color.oninput =  function changeColor(){
+  for (const element of activedrawing) {
+    element.style.backgroundColor = color.value
+  }
+  } 
+
+  function draw(){
+    for (const element of drawblock){
+      element.classList.add('activedrawing')
+    }
+  }
+
+
 
