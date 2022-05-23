3056f0b1a67c65191a9335b6d57aea27000a7fda
Rade473 <r.shekularats@gmail.com>
2022-05-23 16:33:15 +0200

    Finished the etch-a-sketch

--- js.js
+++ js.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
   slidernumber = parseInt(slider.value)
-  createCanvas(slidernumber*slidernumber)
-  draw()
-
+  size=slidernumber*slidernumber
+  createCanvas(size)
 }
 )
+
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+let colorMode = true;
+let rainbowMode;
+let eraserMode;
 
 const canvas = document.getElementById('drawing')
 const slider = document.getElementById('myRange');
 const sizevalue = document.getElementById('sizevalue')
 const clearbtn = document.getElementById('clear')
 const color = document.getElementById('colorpicker')
-const activedrawing = document.getElementsByClassName('activedrawing')
-const drawblock = document.getElementsByClassName('drawingelement')
+const drawingelements = document.querySelectorAll('.drawingelement')
+const colorbtn = document.getElementById('colormode')
+const rainbowbtn = document.getElementById('random')
+const eraserbtn = document.getElementById('eraser')
 
 
 
@@ -23,37 +34,63 @@ slider.oninput = function changeSlider() {
   
   }
 slider.onmouseup = function changeCanvas(){
-  size=slidernumber*slidernumber
     clearCanvas()
     createCanvas(size);
 }
 
-clearbtn.addEventListener('click',clearCanvas)
+clearbtn.addEventListener('click',clearBtn)
+function clearBtn(){
+  clearCanvas()
+  createCanvas(size)
+}
 function clearCanvas(){
    canvas.innerHTML = '';
-   
 }
-
+function draw(e){
+    if (e.type ==='mouseover'&& !mouseDown) return
+    else if (colorMode === true){
+    this.style.backgroundColor = color.value}
+    else if (rainbowMode === true){
+    randomColor = Math.floor(Math.random()*16777215).toString(16)
+    this.style.backgroundColor = '#' + randomColor;
+    }
+    else if(eraserMode === true){
+      this.style.backgroundColor = 'white'
+    }
+}
 function createCanvas(size){
     for (i=0; i<size;i++){
         const div = document.createElement('div');
         canvas.appendChild(div)
         div.classList.add('drawingelement')
+        div.style.backgroundColor = 'none'
+        div.addEventListener('mouseover',draw)
+        div.addEventListener('mousedown', draw)
         document.documentElement.style.setProperty('--size', `${slidernumber}`)
     }
 }
 
-color.oninput =  function changeColor(){
-  for (const element of activedrawing) {
-    element.style.backgroundColor = color.value
-  }
-  } 
+colorbtn.addEventListener('click',enterColorMode)
+function enterColorMode(){
+  colorMode = true;
+  rainbowMode = false;
+  eraserMode = false;
+ 
+}
+rainbowbtn.addEventListener('click',enterRainbowMode)
+function enterRainbowMode(){
+  rainbowMode = true;
+  colorMode = false;
+  eraserMode = false;
+  
+}
+eraserbtn.addEventListener('click',enterEraserMode)
+function enterEraserMode(){
+  eraserMode = true;
+  rainbowMode = false;
+  colorMode = false;
+}
 
-  function draw(){
-    for (const element of drawblock){
-      element.classList.add('activedrawing')
-    }
-  }
-
+color.addEventListener('input',enterColorMode)
 
 
